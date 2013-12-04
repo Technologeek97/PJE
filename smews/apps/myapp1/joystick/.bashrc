f768e0ff300c4d5ebc470aa947bd228b896469ca
Vidal Vincent <vidal@b05p15.fil.univ-lille1.fr>
2013-12-04 14:50:29 +0100

    test comet joy

---
+++ smews/apps/myapp1/joystick/.bashrc
@@ -0,0 +1,1 @@
+export https_proxy=https://cache-etu.univ-lille1.fr:3128
---
+++ smews/apps/myapp1/test_comet1/test.c
@@ -0,0 +1,55 @@
+/*
+<generator>
+	<handlers init="init" initGet="initGet" doGet="doGet"/>
+	<properties persitence="volatile" channel="test"/>
+	<args>
+		<arg name="thres" type="uint16" />
+	</args>
+</generator>
+
+*/
+
+static int joy = 0;
+
+static void timer() {
+
+	if(rflpc_gpio_get_pin(MBED_DIP12) && joy != 3){
+		joy = 3;
+		server_push(&cpuChannel);
+	}
+	if(rflpc_gpio_get_pin(MBED_DIP13) && joy != 4){
+		joy = 4;
+		server_push(&cpuChannel);
+	}
+	if(rflpc_gpio_get_pin(MBED_DIP14) && joy != 5){
+		joy = 5;
+		server_push(&cpuChannel);
+	}
+	if(rflpc_gpio_get_pin(MBED_DIP15) && joy != 1){
+		joy = 1;
+		server_push(&cpuChannel);
+	}
+	if(rflpc_gpio_get_pin(MBED_DIP16) && joy != 2){
+		joy = 2;
+		server_push(&cpuChannel);
+	}
+}
+
+static char initGet(struct args_t *args) {
+	rflpc_pin_set(MBED_DIP12, 0, RFLPC_PIN_MODE_RESISTOR_PULL_DOWN, 0);
+    rflpc_pin_set(MBED_DIP13, 0, RFLPC_PIN_MODE_RESISTOR_PULL_DOWN, 0);
+  	rflpc_pin_set(MBED_DIP14, 0, RFLPC_PIN_MODE_RESISTOR_PULL_DOWN, 0);
+  	rflpc_pin_set(MBED_DIP15, 0, RFLPC_PIN_MODE_RESISTOR_PULL_DOWN, 0);
+  	rflpc_pin_set(MBED_DIP16, 0, RFLPC_PIN_MODE_RESISTOR_PULL_DOWN, 0);
+	return 1;
+}
+
+static char init(void) {
+	
+	return set_timer(&timer,500);
+}
+
+static char doGet(struct args_t *args) {
+	out_uint(joy);
+	return 1;
+}
